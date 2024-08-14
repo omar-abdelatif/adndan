5ad6190c9162804237d617055ffd6259850df626
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2024-08-14 14:21:46 +0300

    update

--- public/assets/backend/js/custom.js
+++ public/assets/backend/js/custom.js
@@ -10,8 +10,32 @@ for (let i = 0; i < 100; i++) {
         pagingTag: "button",
         pagingType: "simple_numbers",
         dom: "Bfrtip",
-        select: true,
-        buttons: ["excelHtml5", "print"],
+        buttons: [
+            { extend: "excelHtml5" },
+            {
+                extend: "print",
+                footer: true,
+                customize: function (win) {
+                    $(win.document.body).prepend('<p class="fw-bold fs-3 text-decoration-underline" style="text-align: center; margin-bottom: 19px">تقرير المصروفات الشهرية</p>').prepend('<div style="text-align: center; margin-bottom: 10px; font-weight: bold;">تم إصداره في تاريخ: ' + new Date().toLocaleDateString('en-GB') + '</div>').prepend('<img src="https://kfala.adendan.com/icons/download.png" style="width: 100px">').prepend('<hr>');
+                    const tfoot = $(win.document.body).find("tfoot");
+                    $(win.document.body).find("body").find("h1").addClass("d-none")
+                    if (tfoot.length > 0) {
+                        tfoot.show();
+                    } else {
+                        const table = $(win.document.body).find("table");
+                        table.append($(this.table().footer().clone()));
+                    }
+                    $(win.document.body).css({
+                        "font-size": "10pt",
+                        "line-height": "1.6",
+                        "direction": 'rtl',
+                        "text-align": "center",
+                    });
+                    $(win.document.body).find("table").addClass("compact mt-4 w-100");
+                    $(win.document.body).find("table").find("th").addClass("text-center");
+                },
+            },
+        ],
     });
 }
 //! Calculate The Year
@@ -34,8 +58,8 @@ errors.forEach((error) => {
 });
 //! Set Timeout To Alert Notification
 const success = document.querySelectorAll(".alert-success");
-success.forEach((error) => {
+success.forEach((suc) => {
     setTimeout(function () {
-        error.style.display = "none";
+        suc.style.display = "none";
     }, 5000);
 });
